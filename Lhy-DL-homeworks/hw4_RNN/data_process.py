6425a424d537c7702d94e375f494bf0e4dfba35c
bljessica <1194620082@qq.com>
2020-12-19 13:42:06 +0800

    continue

--- Lhy-DL-homeworks/hw4_RNN/data_process.py
+++ Lhy-DL-homeworks/hw4_RNN/data_process.py
@@ -12,9 +12,15 @@ class Process():
 
     def get_w2v_model(self):
         # 读取之前训练好的 word2vector 模型
-        self.embedding_matrix = Word2Vec.load(self.w2v_path)
+        self.embedding = Word2Vec.load(self.w2v_path)
         self.embedding_dim = self.embedding.vector_size
-        # self.embedding_dim = self.embedding_matrix.vector_size
 
     def add_embedding(self, word):
-        # vector = 
+        # 把 word 加进embedding，并赋予他一个随机生成的表示向量
+        # word 只会是 <PAD> 或 <UNK>
+        vector = torch.empty(1, self.embedding_dim) # 生成 1 * dim 维的张量
+        torch.nn.init.uniform_(vector)
+        self.word2idx[word] = len(self.word2idx)
+        self.idx2word.append(word)
+        self.embedding_matrix = torch.cat([self.embedding_matrix, vector], 0)
+    
