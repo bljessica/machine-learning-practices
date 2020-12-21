271defe02d384eabc21a777e77d1df8ad41c2871
bljessica <1194620082@qq.com>
2020-12-21 21:38:56 +0800

    add seq2seq model

--- Lhy-DL-homeworks/hw8-seq2seq-translation/model.py
+++ Lhy-DL-homeworks/hw8-seq2seq-translation/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
@@ -56,3 +57,39 @@ class Decoder(nn.Module):
         return prediction, hidden
 
 
+# 当输出过长或单独靠内容向量无法取得整个输入的意思时，用 Attention 机制来提供 Decoder 更多信息
+# 主要是根据现在 Decoder hidden state 去计算 Encoder outputs 中那些与其有较大关系，根据关系的数值来决定该传给 Decoder 的额外信息
+class Attention(mm.Module):
+    def __init__(self, hid_dim):
+        super(Attention, self).__init__()
+        self.hid_dim = hid_dim
+    
+    def forward(self, encoder_outputs, decoder_hidden):
+        # encoder_outputs = [batch_size, seq_len, hid_dim * directions]
+        # decoder_hidden = [num_layers, batch_size, hid_dim]
+        # 一般是取 encoder 最后一层的 hidden state 来做 Attention
+        # TODO：实际Attention机制
+        attention = None
+        return attention
+
+
+# 由 encoder 和 decoder 组成
+class Seq2seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        assert encoder.n_layers == decoder.n_layers
+
+    def forward(self, input, target, teacher_forcing_ratio):
+        # input  = [batch_size, input_len, vocab_size]
+        # target = [batch_size, target_len, vocab_size]
+        batch_size = target.shape[0]
+        target_len = target.shape[1]
+        vocab_size = self.decoder.cn_vocab_size
+
+        outputs = torch.zeros(batch_size, target_len, vocab_size).to(self.device)
+        encoder_outputs, hidden = self.encoder(input)
+        # encoder 的最终隐层 hidden state 用来初始化 decoder
+        hidden = hidden.view(self.encoder.n_layers, 2, )
